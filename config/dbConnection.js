5c4cfbcfbab49079d1a9536a9a8f0e659dbbf998
anstcher <anastasiacher73@gmail.com>
2023-01-22 23:35:59 +0100

    diseño de la pagina de inicio

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql')
+const {HOST, USER, PASSWORD, DATABASE} = require('./config')
 
 module.exports = () =>{
     return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'news_portal'
+        host: HOST,
+        user: USER,
+        password: PASSWORD,
+        database: DATABASE  
     })
 }
--- routes/news.js
+++ routes/news.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const dbConnection = require('../config/dbConnection')
+const connection = dbConnection()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'News' });
-  const connection = dbConnection()
-  connection.query("SELECT * FROM news", (error, result) =>{
+  console.log(req.query)
+
+  let limite = req.query.new-1
+  
+  if(!limite){
+    limite=0
+  }
+
+  console.log("limite",limite)
+  connection.query(`SELECT * FROM news LIMIT ${limite},5`, (error, result) =>{
       
     res.render('news/news', {
         news: result
-        
+
     })
 
   })
 });
+
+router.post('/', function(req, res, next) {
+
+    console.log(req.body)
+    const {title, news} = req.body
+    
+    connection.query('INSERT INTO news SET ?', { title, news }, (error,result) =>{
+        res.redirect('/news')
+    })
+  
+  });
+  
 
 module.exports = router;
